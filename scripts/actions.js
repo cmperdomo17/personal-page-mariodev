1a6b2d8397351b5f4dfe025ed94e136457496e41
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-03-17 15:57:56 -0500

    Actualizar ícono de perfil y animaciones en el menú

--- scripts/actions.js
+++ scripts/actions.js
@@ -12,12 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
   });
     
 
-  // function moveMenu(element){
-  //   element.style.marginTop = "30px";
-  //   element.style.transition = "1s";
-  // }
+function moveMenu(element){
+  element.style.marginTop = "30px";
+  element.style.transition = "1s";
+}
 
-  // function changeOpacity(element){
-  //   element.style.opacity = '1';
-  //   element.style.transition = "2s";
-  // }
+function changeOpacity(element){
+  element.style.opacity = '1';
+  element.style.transition = "2s";
+}
